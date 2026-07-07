c52c4212829a5f5b2cfc9a8845171bdbd7f5f742
maria-tsormpa <mariaeirinitsormpa@gmail.com>
2026-07-07 22:08:27 +0300

    Add files via upload

---
+++ 01_ LCD_LM35/LM35_LCD.ino
@@ -0,0 +1,34 @@
+#include <LiquidCrystal.h>
+
+// οθόνη LCD
+LiquidCrystal lcd(2,3,4,5,6,7);
+
+// LM35 αναλογικός αισθητήρας θερμοκρασίας
+const int LM35_Pin = A0;
+
+void setup() {
+  analogReference(INTERNAL);
+
+  lcd.begin(16,2);
+
+}
+
+void loop() {
+  // υπολογισμός της θερμοκρασίας
+  float temperature;
+
+  float value = analogRead(LM35_Pin);
+
+  temperature = (value*0.85*100.0/1024.0);
+  
+  // εμφάνιση στην οθόνη
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("T: ");
+  lcd.print(temperature);
+  lcd.print((char)223);
+  lcd.print("C");
+
+  delay(1000);
+
+}
